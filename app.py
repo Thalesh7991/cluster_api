88d7c50fb9ded0805ee5da02e502feb247c07b65
Thales Henrique <47004668+Thalesh7991@users.noreply.github.com>
2024-10-31 18:13:21 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, Response
+import os
+import pandas as pd
+import json
+
+from empresa.empresa import CustomerClusteringModel
+
+app = Flask(__name__)
+
+@app.route('/empresa/predict', methods=['POST'])
+def emprestimo_predict():
+    test_json = request.get_json()
+    
+    if test_json:
+        if isinstance(test_json, dict):
+            test_raw = pd.DataFrame(test_json, index=[0])
+        else:
+            test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
+        
+        pipeline = CustomerClusteringModel()
+
+        df_predict = pipeline.predict_cluster(test_raw)
+
+        return df_predict
+    else:
+        return Response('{}', status=200, mimetype='application/json')
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000)
+    app.run('0.0.0.0', port=port)
---
+++ empresa/empresa.py
@@ -0,0 +1,61 @@
+import pickle
+import numpy as np
+import pandas as pd
+import json
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.manifold import TSNE
+# from sklearn.cluster import KMeans
+
+class CustomerClusteringModel:
+    def __init__(self):
+        # Carrega os modelos e objetos necessários do processo de treinamento
+        with open('scaler/scalers.pkl', 'rb') as file:
+            self.scaler = pickle.load(file)
+        with open('model/kmeans_model.pkl', 'rb') as file:
+            self.kmeans = pickle.load(file)
+        with open('reducer/umap_reducer.pkl', 'rb') as file:
+            self.umap = pickle.load(file)
+
+
+    def preprocess_data(self, df2):
+        """Pré-processa os dados usando o scaler carregado."""
+        df2["Spent"] = df2["MntWines"]+ df2["MntFruits"]+ df2["MntMeatProducts"]+ df2["MntFishProducts"]+ df2["MntSweetProducts"]+ df2["MntGoldProds"]
+
+        df2["Children"]=df2["Kidhome"]+df2["Teenhome"]
+        df2["Is_Parent"] = np.where(df2.Children> 0, 1, 0)
+
+        df2["Age"] = 2024-df2["Year_Birth"]
+
+        for col in df2.columns:
+            if col in self.scaler:  # Verifica se o scaler existe para a coluna
+                df2[col] = self.scaler[col].transform(df2[[col]])
+        
+        df2 = df2.drop(columns=['ID','Education', 'Marital_Status', 'Dt_Customer', "Z_CostContact", "Z_Revenue"])
+        df2 = df2[['Year_Birth', 'Income', 'Kidhome', 'Teenhome', 'Recency', 'MntWines',
+                    'MntFruits', 'MntMeatProducts', 'MntFishProducts', 'MntSweetProducts',
+                    'MntGoldProds', 'NumDealsPurchases', 'NumWebPurchases',
+                    'NumCatalogPurchases', 'NumStorePurchases', 'NumWebVisitsMonth',
+                    'AcceptedCmp2', 'Complain', 'Response', 'Spent', 'Children',
+                    'Is_Parent', 'Age']]
+
+        return df2
+    
+    def reduce_dimensionality(self, data):
+        """Reduz a dimensionalidade dos dados usando o modelo TSNE carregado."""
+        data = self.umap.transform(data)
+        data = pd.DataFrame(data, columns=['embedding_x', 'embedding_y'])
+        return data
+    
+    def predict_cluster(self, data):
+        """Pipeline completo para prever o cluster de novos dados."""
+        # Passo 1: Pré-processamento dos dados
+        data_scaled = self.preprocess_data(data)
+
+        
+        # Passo 2: Redução de dimensionalidade
+        data_reduced = self.reduce_dimensionality(data_scaled)
+
+        # Passo 3: Previsão de cluster
+        clusters = self.kmeans.predict(data_reduced)
+
+        return json.dumps(clusters.tolist())
