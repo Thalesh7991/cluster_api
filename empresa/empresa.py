10454968d8fb39031534457ab62966fc8814d39a
Thales Henrique <47004668+Thalesh7991@users.noreply.github.com>
2024-11-04 19:15:10 -0300

    Update empresa.py

--- empresa/empresa.py
+++ empresa/empresa.py
@@ -3,7 +3,7 @@ import json
 
 class CustomerClusteringModel:
     def __init__(self):
-        self.kmeans = pickle.load(open('api/model/kmeans_model.pkl','rb'))
+        self.kmeans = pickle.load(open('model/kmeans_model.pkl','rb'))
         
     
     def predict_cluster(self, data):
