07f01651ebf286410cdd2017d78feb4591a80bb7
Thales Henrique <47004668+Thalesh7991@users.noreply.github.com>
2024-11-04 20:10:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ def emprestimo_predict():
         
         pipeline = CustomerClusteringModel()
 
+        test_raw['embedding_x'] = test_raw['embedding_x'].astype('float64')
+        test_raw['embedding_y'] = test_raw['embedding_y'].astype('float64')
+
         df_predict = pipeline.predict_cluster(test_raw)
 
         return df_predict
