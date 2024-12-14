33d712d9f70d80a1d18d7ffe4eda53fbcdaddaa5
Thales Henrique <47004668+Thalesh7991@users.noreply.github.com>
2024-12-14 16:07:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def emprestimo_predict():
 
         test_raw['embedding_x'] = test_raw['embedding_x'].astype('float64')
         test_raw['embedding_y'] = test_raw['embedding_y'].astype('float64')
+        
 
         df_predict = pipeline.predict_cluster(test_raw)
 
