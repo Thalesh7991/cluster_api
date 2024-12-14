9b7928b87d1e02d089222a2a97d482a8f3fc9556
Thales Henrique <47004668+Thalesh7991@users.noreply.github.com>
2024-12-14 16:06:17 -0300

    Add files via upload

--- empresa/empresa.py
+++ empresa/empresa.py
@@ -1,17 +1,10 @@
 import pickle
 import json
-import numpy as np
 
 class CustomerClusteringModel:
     def __init__(self):
-        self.kmeans = pickle.load(open('model/kmeans_model.pkl','rb'))
-        
+        self.kmeans = pickle.load(open('model/kmeans_model.pkl', 'rb'))
     
     def predict_cluster(self, data):
-        """Pipeline completo para prever o cluster de novos dados."""
-
-        # Passo 3: Previsão de cluster
-        data = data.astype(np.float64)
         clusters = self.kmeans.predict(data)
-        
         return json.dumps(clusters.tolist())
